b45d9de7b254e9045f99d017b53dd52cc3421131
krystal <krystal93002232@gmail.com>
2022-03-20 17:43:40 +0800

    todo

--- 09-TODOApp/app.js
+++ 09-TODOApp/app.js
@@ -1,24 +1,32 @@
-const task = document.querySelector("#taskInput")
-const btn = document.querySelector("#addBtn")
+const taskinput = document.querySelector("#taskInput")
+const addbtn = document.querySelector("#addBtn")
 const todolist = document.querySelector(".todo-list")
 
-btn.addEventListener("click", function (addlist) {
-  const list = task.value
+function todo(todo) {
   const todoitem = document.createElement("li")
   todoitem.classList.add("todo-item")
-  todolist.insertAdjacentElement("afterbegin", todoitem) //span+button //在ul內層的上方
+  todolist.insertAdjacentElement("afterbegin", todoitem)
   const item = document.createElement("span")
   item.classList.add("item")
-  item.textContent = list
-  todoitem.appendChild(item) //追加小孩
   const closebtn = document.createElement("button")
   closebtn.classList.add("closeBtn")
+  todoitem.appendChild(item)
   todoitem.appendChild(closebtn)
+  item.textContent = taskinput.value
   closebtn.textContent = "X"
   const closebutton = document.querySelector(".closeBtn")
-  closebutton.addEventListener("click", function (closelist) {
-    const todoitems = document.querySelector(".todo-item")
-    const deletebtn = closebutton.parentElement
-    deletebtn.remove(closelist)
+  closebutton.addEventListener("click", function (deletelist) {
+    deletelist.target.parentElement.remove(deletelist)
   })
+  taskinput.value = " "
+}
+
+taskinput.addEventListener("keyup", function (enter) {
+  if (enter.keyCode === 13) {
+    todo(todo)
+  }
+})
+
+addbtn.addEventListener("click", function (addlist) {
+  todo(todo)
 })
