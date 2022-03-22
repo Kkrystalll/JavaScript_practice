6e580e8a40aa79bb55f03a7638093b259c86f63a
krystal <krystal93002232@gmail.com>
2022-03-22 23:53:48 +0800

    youbike

--- 09-TODOApp/app.js
+++ 09-TODOApp/app.js
@@ -1,6 +1,6 @@
-const taskinput = document.querySelector("#taskInput")
-const addbtn = document.querySelector("#addBtn")
-const todolist = document.querySelector(".todo-list")
+// const taskinput = document.querySelector("#taskInput")
+// const addbtn = document.querySelector("#addBtn")
+// const todolist = document.querySelector(".todo-list")
 
 function todo(todo) {
   const todoitem = document.createElement("li")
@@ -21,12 +21,68 @@ function todo(todo) {
   taskinput.value = " "
 }
 
-taskinput.addEventListener("keyup", function (enter) {
-  if (enter.keyCode === 13) {
-    todo(todo)
-  }
-})
+// taskinput.addEventListener("keyup", function (enter) {
+//   // if (enter.key === "Enter")
+//   if (enter.keyCode === 13) {
+//     todo(todo)
+//   }
+// })
 
-addbtn.addEventListener("click", function (addlist) {
-  todo(todo)
+// addbtn.addEventListener("click", function (addlist) {
+//   todo(todo)
+// })
+
+function createTodoItem(text) {
+  const todo = document.createElement("li")
+  todo.className = "todo-item"
+
+  const span = document.createElement("span")
+  span.className = "item"
+  span.textContent = text
+
+  const btn = document.createElement("button")
+  btn.className = "closeBtn"
+  btn.textContent = "X"
+
+  todo.appendChild(span)
+  todo.appendChild(btn)
+
+  return todo
+}
+
+function addItemInContainer(container, input) {
+  if (input.value.trim() !== "") {
+    const todo = createTodoItem(input.value)
+    container.insertAdjacentElement("afterbegin", todo)
+  }
+
+  input.value = ""
+  input.focus()
+}
+
+function addTodo() {
+  const container = document.querySelector(".todo-list")
+  const input = document.querySelector("#taskInput")
+  addItemInContainer(container, input)
+}
+
+// document.querySelector("#addBtn").addEventListener("click", addTodo)
+$("#addBtn").click(addTodo) //jQuery
+
+// document.querySelector("#taskInput").addEventListener("keyup", (e) => {
+//   if (e.key === "Enter") {
+//     addTodo()
+//   }
+// })
+$("#taskInput").keyup((e) => {
+  if (e.key === "Enter") {
+    addTodo()
+  }
+}) //jQuery寫法
+
+// 事件委任 event delegation
+document.querySelector(".todo-list").addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+    e.target.parentElement.remove()
+  }
 })
--- 10-YouBike即時資訊/app.js
+++ 10-YouBike即時資訊/app.js
@@ -1,1 +1,52 @@
-// 程式碼寫這裡
+const API =
+  "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+
+const input = document.querySelector("#searchKeyword")
+const button = document.querySelector(".btn.btn-success.fs-3")
+const ul = document.querySelector(".siteList.list-group")
+
+async function getplace(keyword) {
+  const rawdata = await fetch(API)
+  const ubikedata = await rawdata.json()
+  // console.log(keyword)
+  const roadubike = ubikedata.filter((data) => data["ar"].includes(keyword))
+  roadubike.forEach((place) => {
+    const station = place.sna.slice(11)
+    const bike = place.sbi
+    const road = place.ar
+    // console.log(station)
+    const list = document.createElement("li")
+    list.classList.add("list-group-item", "fs-5")
+    ul.appendChild(list)
+    list.textContent = `${station} (${bike})`
+    const br = document.createElement("br")
+    list.appendChild(br)
+
+    const small = document.createElement("small")
+    list.appendChild(small)
+    small.textContent = road
+
+    const icon = document.createElement("i")
+    icon.className = "fas fa-bicycle"
+    // list.appendChild(icon)
+    list.insertAdjacentElement("afterbegin", icon)
+    // console.log(getplace())
+  })
+}
+
+button.addEventListener("click", (e) => {
+  e.preventDefault()
+  let keyword = input.value
+
+  getplace(keyword)
+
+  input.value = ""
+  input.focus()
+  ul.textContent = ""
+})
+
+input.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    getplace()
+  }
+})
